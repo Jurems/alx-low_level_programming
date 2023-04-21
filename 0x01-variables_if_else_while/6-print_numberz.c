2b14cd927cd632816c06dce6f4209133902d802a
Jurems <jenniferoleju@gmail.com>
2023-04-21 16:25:02 -0700

    Priting digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Printing single digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
