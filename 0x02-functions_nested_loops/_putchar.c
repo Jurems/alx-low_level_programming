52c027afd81c4d00257f285264ca7c94dddecc3e
Jurems <jenniferoleju@gmail.com>
2023-04-25 19:45:37 -0700

    pushing putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c is the character to print
+ * return on sucess 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
