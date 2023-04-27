bc6e4315523d042d71c17a021e2ec413e4c1f737
Jurems <jenniferoleju@gmail.com>
2023-04-27 12:06:13 -0700

    0-main.c  1-main  1-main.c  a.out  main.c  main.h  positive_or_negative.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* main - test for a postitive_or_negative()
+* main - Test function for postitive_or_negative()
 * Return: 0
 */
 
 int main(void)
 {
 int i;
-i = 0
-postitive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 return (0);
 }
